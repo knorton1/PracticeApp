b435c6e68993a6371ff77f17f28dd9666e8c1913
knorton1 <88497017+knorton1@users.noreply.github.com>
2023-01-12 21:08:13 -0500

    Initial Commit

---
+++ PracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeApp
+//
+//  Created by Katelyn Norton on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
